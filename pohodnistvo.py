ca9ad5c2e4b5c12c86bd443011ee78ac6f238478
urbanmerhar <merhar.urban@gmail.com>
2020-04-30 14:20:22 +0200

    začetek

---
+++ pohodnistvo.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+# uvozimo bottle.py
+from bottle import get, post, run, request, template, redirect, static_file
+
+# uvozimo ustrezne podatke za povezavo
+# import auth_public as auth
+
+# uvozimo psycopg2
+# import psycopg2, psycopg2.extensions, psycopg2.extras
+# psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+import os
+
+# privzete nastavitve
+SERVER_PORT = os.environ.get('BOTTLE_PORT', 8080)
+RELOADER = os.environ.get('BOTTLE_RELOADER', True)
+ROOT = os.environ.get('BOTTLE_ROOT', '/')
+DB_PORT = os.environ.get('POSTGRES_PORT', 5432)
+
+# odkomentiraj, če želiš sporočila o napakah
+# debug(True)
+
+# OSNOVNE STRANI
+
+def rtemplate(*largs, **kwargs):
+    """
+    Izpis predloge s podajanjem spremenljivke ROOT z osnovnim URL-jem.
+    """
+    return template(ROOT=ROOT, *largs, **kwargs)
+
+@get('/')
+def osnovna_stran():
+    redirect('/pohodnistvo')
+
+@get('/pohodnistvo')
+def glavna_stran():
+    return rtemplate('glavna_stran.html')
+
+@get('/osebe')
+def osebe():
+    return rtemplate('osebe.html')
+
+@get('/gore')
+def gore():
+    return rtemplate('gore.html')
+
+# Za STATIC datoteke(slike)
+
+@get('/static/<filename:path>')
+def static(filename):
+    return static_file(filename, root='static')
+
+# O PROJEKTU
+
+@get('/o_projektu')
+def o_projektu():
+    return rtemplate('o_projektu.html')
+
+######################################################################
+# Glavni program
+
+# priklopimo se na bazo
+# conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, password=auth.password, port=DB_PORT)
+# conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT) # onemogočimo transakcije
+# cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+# poženemo strežnik na podanih vratih, npr. http://localhost:8080/
+run(host='localhost', port=SERVER_PORT, reloader=RELOADER)
