a477375878616a5c6de31502b487cfa001770a58
Urban Merhar <merhar.urban@gmail.com>
2020-07-30 14:16:08 +0200

    Še par popravkov in šmekanja tabel.

--- static/pohodnistvo.js
+++ static/pohodnistvo.js
@@ -2,23 +2,23 @@
 
 // Funkcija za iskanje v tabeli
 function funkcija_iskanja_po_tabeli() {
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("vnos_iskanja");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tabela");
-    tr = table.getElementsByTagName("tr");
-  
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("vnos_iskanja");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("tabela");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
       }
     }
   }
+}
